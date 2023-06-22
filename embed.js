252be8e6b24e7bbb09d0ff4fa7b0e9f697e111dd
ck-siddharth <siddharth.s@codeking.in>
2023-06-22 14:26:14 +0530

    second commit

--- embed.js
+++ embed.js
@@ -1,21 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var headings = [
-      "Heading 1",
-      "Heading 2",
-      "Heading 3",
-      "Heading 4",
-      "Heading 5"
-    ];
+    var data = {
+      "001": [
+        "Heading 1",
+        "Heading 2",
+        "Heading 3",
+        "Heading 4",
+        "Heading 5"
+      ],
+      "002": [
+        "Title 6",
+        "Title 7",
+        "Title 8",
+        "Title 9",
+        "Title 10"
+      ]
+    };
   
-    var headingsElement = document.getElementById("headings");
-    var ulElement = document.createElement("ul");
+    var elements = document.querySelectorAll('[data-id]');
   
-    headings.forEach(function(heading) {
-      var liElement = document.createElement("li");
-      liElement.textContent = heading;
-      ulElement.appendChild(liElement);
+    elements.forEach(function(element) {
+      var dataId = element.getAttribute("data-id");
+      var headings = data[dataId];
+  
+      if (headings) {
+        var ulElement = document.createElement("ul");
+  
+        headings.forEach(function(heading) {
+          var liElement = document.createElement("li");
+          liElement.textContent = heading;
+          ulElement.appendChild(liElement);
+        });
+  
+        element.appendChild(ulElement);
+      }
     });
-  
-    headingsElement.appendChild(ulElement);
   });
   
